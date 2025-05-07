bcfbac35cfd893f7edc6b4aa70da63ef95e628cc
D4ryl00 <d4ryl00@gmail.com>
2025-05-07 16:22:50 +0200

    fix: implement the Protocol in Swift
    Signed-off-by: D4ryl00 <d4ryl00@gmail.com>

--- expo/ios/SecureStorage.swift
+++ expo/ios/SecureStorage.swift
@@ -7,41 +7,41 @@
 
 import GnoCore
 
-public class SecureStorage: GnoGnonativeSecureNativeStorage {
-    public override func get(_ p0: Data?) -> Data? {
+public class SecureStorage: NSObject, GnoGnonativeSecureNativeStorageProtocol {
+    public func get(_ p0: Data?) -> Data? {
         return nil
     }
     
-    public override func has(_ key: Data?) -> Bool {
+    public func has(_ key: Data?) -> Bool {
         NSLog("secureStorage: has: key:" + (String(data: key ?? Data(), encoding: .utf8) ?? ""))
         return false
     }
     
-    public override func set(_ p0: Data?, p1: Data?) {
+    public func set(_ p0: Data?, p1: Data?) {
         NSLog("secureStorage: set: key:" + (String(data: p0 ?? Data(), encoding: .utf8) ?? "") + " value:" + (String(data: p1 ?? Data(), encoding: .utf8) ?? ""))
     }
     
-    public override func setSync(_ p0: Data?, p1: Data?) {
+    public func setSync(_ p0: Data?, p1: Data?) {
         NSLog("secureStorage: setSync: key:" + (String(data: p0 ?? Data(), encoding: .utf8) ?? "") + " value:" + (String(data: p1 ?? Data(), encoding: .utf8) ?? ""))
     }
     
-    public override func delete(_ p0: Data?) {
+    public func delete(_ p0: Data?) {
         NSLog("secureStorage: delete: key:" + (String(data: p0 ?? Data(), encoding: .utf8) ?? ""))
     }
     
-    public override func deleteSync(_ p0: Data?) {
+    public func deleteSync(_ p0: Data?) {
         NSLog("secureStorage: deleteSync: key:" + (String(data: p0 ?? Data(), encoding: .utf8) ?? ""))
     }
     
-    public override func write() {
+    public func write() {
         NSLog("secureStorage: write")
     }
     
-    public override func writeSync() {
+    public func writeSync() {
         NSLog("secureStorage: writeSync")
     }
     
-    public override func close() {
+    public func close() {
         NSLog("secureStorage: close")
     }
 }
